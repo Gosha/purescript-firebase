e1febaa471de6722a11c4bb03b5e687737227ab1
Georgij Zacharov <gosha.za@gmail.com>
2022-03-13 20:49:59 +0100

    Remove unused throw

--- src/Web/Firebase/Aff/Read.purs
+++ src/Web/Firebase/Aff/Read.purs
@@ -3,23 +3,15 @@ module Web.Firebase.Aff.Read
   onceValue
 , valueAt
 ) where
-import Prelude (pure, bind, ($), (<<<))
+import Prelude (pure, bind, ($))
 
 import Foreign (Foreign)
 import Effect.Aff (Aff)
-import Effect.Exception (error)
-import Control.Monad.Error.Class (throwError)
 
 import Web.Firebase as FB
 import Web.Firebase.Types as FBT
 import Web.Firebase.DataSnapshot (val)
 import Web.Firebase.Aff (once)
-
--- | Inspired by its Eff relative.
--- | Throw takes a message and throws a MonadError in Aff with that message
--- TODO move to its own module, also used by 'regular' Aff module
-throw :: forall a. String -> Aff a
-throw = throwError <<< error
 
 valueAt :: FBT.Firebase -> Aff Foreign
 valueAt ref = do
